85d52aa9349803847d3d7b1c5a6b0430527b3d87
Rose Francis <rosennabs@gmail.com>
2024-02-10 12:40:33 -0700

    implement the game  class

---
+++ Classes/game.rb
@@ -0,0 +1,57 @@
+require_relative './player'
+require_relative './question'
+
+# Manage the overall game flow
+class Game
+  def initialize(player1, player2)
+    @players = [player1, player2]
+    @current_player_index = 0
+  end
+
+  def start
+    puts "STARTING GAME ..."
+    while !game_over?
+      current_player = @players[@current_player_index]
+      other_player = @players[1 - @current_player_index]
+
+      puts "#{current_player.name}'s turn:"
+      question = Question.new
+      puts question.question
+      print '>'
+      answer = gets.chomp.to_i
+
+      if answer == question.answer
+        puts "Correct! #{current_player.name} gains a point."
+      else
+        puts "Wrong! #{current_player.name} loses a life."
+        current_player.lose_life
+      end
+
+      display_scores
+      switch_turn
+
+    end
+
+    puts "GAME OVER!"
+    winner = @players.find {|player| player.lives > 0}
+    puts "#{winner.name} wins with #{winner.lives} lives remaining!"
+
+  end
+
+
+
+
+  def game_over?
+    @players.any? {|player| player.lives <= 0}
+  end
+
+  def display_scores
+    puts "Final Scores:"
+    @players.each {|player| puts "#{player.name}: #{player.lives} lives"}
+  end
+
+  def switch_turn
+    @current_player_index = 1 - @current_player_index
+  end
+
+end
--- Classes/question.rb
+++ Classes/question.rb
@@ -7,10 +7,13 @@ class Question
     # generate two random numbers from 1-20
     @num1 = rand(1..20)
     @num2 = rand(1..20)
-    
+
     @question = "#{@num1} plus #{@num2} equals what?"
     @answer = @num1 + @num2
+
 
   end
 
 end
+
+
