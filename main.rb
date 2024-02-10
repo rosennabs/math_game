6181108e74f1d0a2f173eed908a066af19bd3edf
Rose Francis <rosennabs@gmail.com>
2024-02-10 00:43:26 -0700

    initialize players and game in main

--- main.rb
+++ main.rb
@@ -2,3 +2,12 @@
 require_relative './Classes/player'
 require_relative './Classes/game'
 require_relative './Classes/questions'
+
+# Initialize players
+player1 = Player.new('Player 1')
+player2 = Player.new('Player 2')
+
+# Initialize game
+game = Game.new(player1, player2)
+
+
